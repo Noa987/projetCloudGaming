1556e16762ae714503ec1f76c744265c13c6f15c
noa987 <noa.mourareau@gmail.com>
2023-04-05 11:14:48 +0200

    fablab + register and login

--- Assets/FoxAdventures/Cours/Scripts/Auth/PlayfabAuth.cs
+++ Assets/FoxAdventures/Cours/Scripts/Auth/PlayfabAuth.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public static class PlayfabAuth
 {
@@ -13,8 +15,18 @@ public static class PlayfabAuth
     {
         get
         {
-            // TODO: Implement check that we are logged in
-            return false;
+            if (PlayFabClientAPI.IsClientLoggedIn())
+            {
+                Debug.Log("User is logged in");
+                // Do something here if the user is logged in
+                return true;
+            }
+            else
+            {
+                Debug.Log("User is not logged in");
+                // Do something here if the user is not logged in
+                return false;
+            }
         }
     }
 
@@ -26,18 +38,50 @@ public static class PlayfabAuth
 
     public static void TryRegisterWithEmail(string email, string password, string username, Action registerResultCallback, Action errorCallback)
     {
-        // TODO: Request playfab for registration
         // --------------------------------------
         // >> For the moment, we will consider it to be a succes
-        registerResultCallback.Invoke();
+        // Create a new RegisterPlayFabUserRequest
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = email,
+            Password = password,
+            Username = username
+        };
+
+        // Call RegisterPlayFabUser with the request
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+        //registerResultCallback.Invoke();
+    }
+
+    public static void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("Registration successful");
+        // Do something here if the registration was successful
+    }
+
+    public static void OnError(PlayFabError error)
+    {
+        Debug.LogError($"Error: {error.ErrorMessage}");
+        // Do something here if there was an error
     }
 
     public static void TryLoginWithEmail(string email, string password, Action loginResultCallback, Action errorCallback)
     {
-        // TODO: Request playfab for login
         // -------------------------------
         // >> For the moment, we will consider it to be a success
-        loginResultCallback.Invoke();
+        //loginResultCallback.Invoke();
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = email,
+            Password = password
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+    }
+
+    public static void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Login successful");
+        // Do something here if the login was successful
     }
 
     // Logout
